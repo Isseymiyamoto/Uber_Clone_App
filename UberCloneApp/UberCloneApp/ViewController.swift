9e7397fcdeecadf9a12584d4e88a2facd2705242
Isseymiyamoto <ants.can.walk@gmail.com>
2020-09-12 23:02:18 +0900

    make a new project

---
+++ UberCloneApp/UberCloneApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberCloneApp
+//
+//  Created by 宮本一成 on 2020/09/12.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
