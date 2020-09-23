b9d9a1d90f02bee5af3f6d958793dde940a9272d
Isseymiyamoto <ants.can.walk@gmail.com>
2020-09-23 22:49:26 +0900

    update driver position in real time

--- UberCloneApp/UberCloneApp/Model/DriverAnnotation.swift
+++ UberCloneApp/UberCloneApp/Model/DriverAnnotation.swift
@@ -9,11 +9,17 @@
 import MapKit
 
 class DriverAnnotation: NSObject, MKAnnotation{
-    var coordinate: CLLocationCoordinate2D
+    dynamic var coordinate: CLLocationCoordinate2D
     var uid: String
     
     init(uid: String, coordinate: CLLocationCoordinate2D){
         self.coordinate = coordinate
         self.uid = uid
+    }
+    
+    func updateAnnotationPosition(withCoordinate coordinate: CLLocationCoordinate2D){
+        UIView.animate(withDuration: 0.2) {
+            self.coordinate = coordinate
+        }
     }
 }
