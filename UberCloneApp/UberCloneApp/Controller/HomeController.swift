d45945b23f21a09be191265353e94ef9fa8bd655
Isseymiyamoto <ants.can.walk@gmail.com>
2020-09-29 22:16:42 +0900

    configure action view UI

--- UberCloneApp/UberCloneApp/Controller/HomeController.swift
+++ UberCloneApp/UberCloneApp/Controller/HomeController.swift
@@ -30,6 +30,7 @@ class HomeController: UIViewController{
     private let locationManager = LocationHandler.shared.locationManager
     
     private let inputActivationView = LocationInputActivationView()
+    private let rideActionView = RideActionView()
     private let locationInputView = LocationInputView()
     private let tableView = UITableView()
     private var searchResults = [MKPlacemark]()
@@ -157,6 +158,7 @@ class HomeController: UIViewController{
     
     func configureUI(){
         configureMapView()
+        configureActionView()
         
         view.addSubview(actionButton)
         actionButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor,
@@ -200,6 +202,11 @@ class HomeController: UIViewController{
                 self.tableView.frame.origin.y = self.locationInputViewHeight
             }
         }
+    }
+    
+    func configureActionView(){
+        view.addSubview(rideActionView)
+        rideActionView.frame = CGRect(x: 0, y: view.frame.height - 300, width: view.frame.width, height: 300)
     }
     
     func configureTableView(){
