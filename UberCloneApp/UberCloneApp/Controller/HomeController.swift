6fee4b409090ca2f59cae2e6058fd4b474c714c5
Isseymiyamoto <ants.can.walk@gmail.com>
2020-10-09 22:48:20 +0900

    disimissing loading view after trip accepted

--- UberCloneApp/UberCloneApp/Controller/HomeController.swift
+++ UberCloneApp/UberCloneApp/Controller/HomeController.swift
@@ -45,6 +45,7 @@ class HomeController: UIViewController{
             if user?.accountType == .passenger{
                 fetchDrivers()
                 configureLocationInputActivationView()
+                observeCurrentTrip()
             }else{
                 observeTrips()
             }
@@ -53,11 +54,17 @@ class HomeController: UIViewController{
     
     private var trip: Trip? {
         didSet{
-            guard let trip = trip else { return }
-            let controller = PickupController(trip: trip)
-            controller.delegate = self
-            controller.modalPresentationStyle = .fullScreen
-            self.present(controller, animated: true, completion: nil)
+            guard let user = user else { return }
+            
+            if user.accountType == .driver{
+                guard let trip = trip else { return }
+                let controller = PickupController(trip: trip)
+                controller.delegate = self
+                controller.modalPresentationStyle = .fullScreen
+                self.present(controller, animated: true, completion: nil)
+            }else{
+                print("DEBUG: show ride action view for accepted trip..")
+            }
         }
     }
     
@@ -82,7 +89,6 @@ class HomeController: UIViewController{
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        guard let trip = trip else { return }
     }
     
     // MARK: - selectors
@@ -105,6 +111,16 @@ class HomeController: UIViewController{
     
     
     // MARK: - API
+    
+    func observeCurrentTrip(){
+        Service.shared.observeCurrentTrip { (trip) in
+            self.trip = trip
+            
+            if trip.state == .accepted{
+                self.shouldPresentLoadingView(false)
+            }
+        }
+    }
     
     func fetchUserData(){
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
