44ec9cf1ceaf188a39ae11dbd6fa6689d99b1757
Isseymiyamoto <ants.can.walk@gmail.com>
2020-10-10 22:15:41 +0900

    configure ride action view for passenger and driver

--- UberCloneApp/UberCloneApp/Controller/HomeController.swift
+++ UberCloneApp/UberCloneApp/Controller/HomeController.swift
@@ -118,6 +118,11 @@ class HomeController: UIViewController{
             
             if trip.state == .accepted{
                 self.shouldPresentLoadingView(false)
+                guard let driverUid = trip.driverUid else { return }
+                
+                Service.shared.fetchUserData(uid: driverUid) { (driver) in
+                    self.animateRideActionView(shouldShow: true, config: .tripAccepted, user: driver)
+                }
             }
         }
     }
@@ -279,7 +284,8 @@ class HomeController: UIViewController{
         }, completion: completion)
     }
     
-    func animateRideActionView(shouldShow: Bool, destination: MKPlacemark? = nil, config: RideActionViewConfiguration? = nil){
+    func animateRideActionView(shouldShow: Bool, destination: MKPlacemark? = nil,
+                               config: RideActionViewConfiguration? = nil, user: User? = nil){
         let yOrigin = shouldShow ? self.view.frame.height - rideActionViewHeight : self.view.frame.height
         
         UIView.animate(withDuration: 0.3) {
@@ -288,10 +294,16 @@ class HomeController: UIViewController{
         
         if shouldShow{
             guard let config = config else { return }
-            rideActionView.configureUI(withConfig: config)
             
-            guard let destination = destination else { return }
-            rideActionView.destination = destination
+            if let destination = destination{
+                rideActionView.destination = destination
+            }
+            
+            if let user = user{
+                rideActionView.user = user
+            }
+            
+            rideActionView.configureUI(withConfig: config)
         }
     }
 }
@@ -465,7 +477,7 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource{
             let annotations = self.mapView.annotations.filter({ !$0.isKind(of: DriverAnnotation.self )})
             self.mapView.zoomToFit(annotations: annotations)
             
-            self.animateRideActionView(shouldShow: true, destination: selectedPlacemark)
+            self.animateRideActionView(shouldShow: true, destination: selectedPlacemark, config: .requestRide)
         }
         
     }
@@ -509,7 +521,9 @@ extension HomeController: PickupControllerDelegate{
         mapView.zoomToFit(annotations: mapView.annotations)
         
         self.dismiss(animated: true) {
-            self.animateRideActionView(shouldShow: true, config: .tripAccepted)
+            Service.shared.fetchUserData(uid: trip.passengerUid) { (passenger) in
+                self.animateRideActionView(shouldShow: true, config: .tripAccepted, user: passenger)
+            }
         }
     }
 }
